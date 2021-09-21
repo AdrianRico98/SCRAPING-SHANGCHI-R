39c5453e3606072047500ca3d737d0c5b4368667
ADRIÁN RICO <82446517+AdrianRico98@users.noreply.github.com>
2021-09-21 19:11:46 +0200

    Update EDA.R
    .

--- EDA.R
+++ EDA.R
@@ -1,4 +1,4 @@
-#1 .En cualquier momento queinterese, importamos los datos actualizados en el mismo día
+#1 .En cualquier momento que interese, importamos los datos actualizados en el mismo dÃ­a
 library(readxl)
 shang_chi <- read_excel("shang_chi.xlsx") #es recomendable cambiar el working directory a donde se encuentra el proyecto para no escribir rutas completas
 
@@ -6,23 +6,23 @@ shang_chi <- read_excel("shang_chi.xlsx") #es recomendable cambiar el working di
 library(tidyverse)
 library(ggrepel)
 library(ggthemes)
-#2.1 ¿En qué región esta consiguiendo mas ingresos la película? ¿y menos? ¿ Cómo se estan comportando, en términos de ingresos, los distintos mercados de las regiones?
+#2.1 Â¿En quÃ© regiÃ³n esta consiguiendo mas ingresos la pelÃ­cula? Â¿y menos? Â¿ CÃ³mo se estan comportando, en tÃ©rminos de ingresos, los distintos mercados de las regiones?
 shang_chi %>% 
   ggplot(aes(Region,`Ingresos Totales`, col = Region)) + 
   scale_y_continuous(trans = "log2") +
   geom_point(aes()) +
   xlab(" Regiones")+
-  ylab("Ingresos Totales hasta hoy (escala logarítmica)")+
+  ylab("Ingresos Totales hasta hoy (escala logarÃ­tmica)")+
   theme(legend.position = "none") +
-  ggtitle("SHANG CHI: Comparación del rendimiento de la película entre/intra regiones") +
+  ggtitle("SHANG CHI: ComparaciÃ³n del rendimiento de la pelÃ­cula entre/intra regiones") +
   geom_text_repel(aes(Region, label = Mercado), nudge_y = 0.15, size = 4) +
   theme_solarized_2()
-#2.2 ¿Qué peso tienen las distintas regiones en los ingresos totales?
-## Los ingresos totales a nivel mundial de la película son:
+#2.2 Â¿QuÃ© peso tienen las distintas regiones en los ingresos totales?
+## Los ingresos totales a nivel mundial de la pelÃ­cula son:
 ingresos_mundiales <- shang_chi %>% 
   summarize(ingresos_mundiales = sum(`Ingresos Totales`)) %>% 
   pull (ingresos_mundiales)
-## calculo el peso respecto a los ingresos mundiales por region:
+## Calculo el peso respecto a los ingresos mundiales por region:
 shang_chi %>% 
   group_by(Region) %>% 
   summarize(ingresos_por_region = sum(`Ingresos Totales`)) %>%
